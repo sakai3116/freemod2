e7f287a8ae761b35d7849ccba7cc882a2d46de33
sakai3116 <kalarbalest3116@gmail.com>
2025-10-06 13:18:54 +0900

    add[鉱石の自動生成]

--- FREEMOD ver1.2/test/src/main/java/com/FREEMOD/freemod/world/ModWorldEvents.java
+++ FREEMOD ver1.2/test/src/main/java/com/FREEMOD/freemod/world/ModWorldEvents.java
@@ -1,6 +1,7 @@
 package com.FREEMOD.freemod.world;
 
 import com.FREEMOD.freemod.FreeMod;
+import com.FREEMOD.freemod.world.gen.ModOreGeneration;
 import com.FREEMOD.freemod.world.gen.ModTreeGeneration;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,6 +13,6 @@ public class ModWorldEvents {
     public static void biomeLoadingEvent(final BiomeLoadingEvent event){
         ModTreeGeneration.generateTree(event);//part 32
         //ModFlowerGeneration.generateFlowers(event);//part 33
-        //ModOreGeneration.generateOres(event);//part 34
+        ModOreGeneration.generateOres(event);//part 34
     }
 }
--- FREEMOD ver1.2/test/src/main/java/com/FREEMOD/freemod/world/feature/ModPlacedFeatures.java
+++ FREEMOD ver1.2/test/src/main/java/com/FREEMOD/freemod/world/feature/ModPlacedFeatures.java
@@ -13,6 +13,11 @@ public class ModPlacedFeatures {
                     PlacementUtils.countExtra(3,0.1F,2)));
     //おそらくチャンクあたりの木の生成数を指定するもの。0.1F＝10％で生成確率
 
+    //プラチナ原石の生成
+    public static final Holder<PlacedFeature> PLATINUM_ORE_PLACED = PlacementUtils.register("platinum_ore/placed",
+            ModConfiguredFeatures.PLATINUM_ORE,ModOrePlacement.commonOrePlacement(7,
+                    HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-80),VerticalAnchor.aboveBottom(80))));
+
 //    //part 33　花の生成場所など
 //    public static final Holder<PlacedFeature> PINK_ROSE_PLACED = PlacementUtils.register("pink_rose_placed",
 //            ModConfiguredFeatures.PINK_ROSE, RarityFilter.onAverageOnceEvery(16), //バラのフィルターが16ごとに配置されている？
